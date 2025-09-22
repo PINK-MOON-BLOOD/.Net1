5d024cad504ecc94f9e5ceba472a61ab176a3edc
_PASHA_ <167881594+PINK-MOON-BLOOD@users.noreply.github.com>
2025-09-22 22:33:48 +0300

    Finished work with Domain.Models. and Domain.Logic.

---
+++ Paul1000.TaskPlanner.Domain.Models/Enums/Complexity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paul1000.TaskPlanner.Domain.Models_.Enums
+{
+    // Complexity levels for tasks
+    public enum Complexity
+    {
+        None,
+        Minutes,
+        Hours,
+        Days,
+        Weeks
+    }
+}
---
+++ Paul1000.TaskPlanner.Domain.Models/Enums/Priority.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paul1000.TaskPlanner.Domain.Models_.Enums
+{
+    //  Priority levels for tasks
+    public enum Priority
+    {
+        None,
+        Low,
+        Medium,
+        High,
+        Urgent
+    }
+}
